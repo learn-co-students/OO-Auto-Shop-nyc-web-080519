ee7579d13f273bec54669c10202fc4f39246859f
aresnik11 <aresnik11@gmail.com>
2019-08-12 22:16:55 -0400

    Done.

--- app/models/car_owner.rb
+++ app/models/car_owner.rb
@@ -1,10 +1,34 @@
 class CarOwner
 
   attr_reader :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def my_cars
+    Car.all.select do |car|
+      car.owner == self
+    end
+  end
+
+  def my_mechanics
+    self.my_cars.map do |car|
+      car.mechanic
+    end
+  end
+
+  def self.avg_cars_owned
+    total_cars = self.all.sum do |car_owner|
+      car_owner.my_cars.count
+    end
+    total_cars.to_f / self.all.count
+  end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,18 @@ def reload
   load 'config/environment.rb'
 end
 
+co1 = CarOwner.new("Amy")
+co2 = CarOwner.new("Steven")
+co3 = CarOwner.new("Howard")
+
+m1 = Mechanic.new("Bob", "regular")
+m2 = Mechanic.new("John", "exotic")
+m3 = Mechanic.new("Joe", "antique")
+m4 = Mechanic.new("Shmoe", "antique")
+
+c1 = Car.new("Volvo", "XC-90", "regular", co3, m1)
+c2 = Car.new("Acura", "TL", "antique", co1, m3)
+c3 = Car.new("Tesla", "model III", "exotic", co2, m2)
+c4 = Car.new("Tesla", "model III", "exotic", co1, m2)
+
 binding.pry
